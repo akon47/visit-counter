b2c3966597a9c5393cfcec770d049c55dba9f8a2
Kim Hwan <akon47@naver.com>
2021-08-03 13:43:32 +0900

    svg 생성하는 부분 함수로 분리

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 const visitorModel = require('./models/VisitorModel.js');
 const url = require('url');
 
+const createCounterSvg = (title, text) => {
+    const title = "방문자";
+    const titleTextWidth = 45;
+    const counterTextWidth = 45 + ((text.length - 5) * 6);
+
+    const result =
+        `
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${titleTextWidth + counterTextWidth}" height="20">
+            <linearGradient id="smooth" x2="0" y2="100%">
+                <stop offset="0" stop-color="#bbb" stop-opacity="0.1" />
+                <stop offset="1" stop-opacity="0.1" />
+            </linearGradient>
+
+            <mask id="round">
+                <rect width="${titleTextWidth + counterTextWidth}" height="20" rx="3" ry="3" fill="#fff" />
+            </mask>
+
+            <g mask="url(#round)">
+                <rect width="${titleTextWidth}" height="20" fill="#555" />
+                <rect x="${titleTextWidth}" width="${counterTextWidth}" height="20" fill="#79c83d" />
+                <rect width="${titleTextWidth + counterTextWidth}" height="20" fill="url(#smooth)" />
+            </g>
+
+            <g fill="#fff" text-anchor="middle" font-family="Verdana,DejaVu Sans,Geneva,sans-serif" font-size="11">
+                <text x="${(titleTextWidth / 2.0) + 1}" y="15" fill="#010101" fill-opacity="0.3">${title}</text>
+                <text x="${(titleTextWidth / 2.0) + 1}" y="14" fill="#fff">${title}</text>
+                <text x="${titleTextWidth + (counterTextWidth / 2.0) - 1}" y="15" fill="#010101" fill-opacity="0.3"> ${text} </text>
+                <text x="${titleTextWidth + (counterTextWidth / 2.0) - 1}" y="14" fill="#fff"> ${text} </text>
+            </g>
+        </svg>
+        `;
+    return result;
+};
+
 require('./db.js')();
 const server = require('http').createServer(async (request, response) => {
     if (request.method === 'GET') {
-        const queryObject = url.parse(request.url, true).query;
+        const queryObject = new URL(request.url, true).query;
         if (queryObject.key) {
             const address = request.headers['x-forwarded-for']?.split(',').shift() || request.socket?.remoteAddress;
 
@@ -16,7 +50,7 @@ const server = require('http').createServer(async (request, response) => {
             if (!(await visitorModel
                 .findOne({ address: address, key: queryObject.key, visitedDate: { $gte: startUtc, $lt: endUtc } })
                 .lean()
-                .exec())) {
+                .exec())) { // 오늘 하루동안 방문한적이 없는 IP 라면 DB에 추가.
                 await visitorModel.create({
                     address: address,
                     key: queryObject.key,
@@ -34,39 +68,8 @@ const server = require('http').createServer(async (request, response) => {
                 .lean()
                 .exec() || 0;
 
-            const title = "방문자";
-            const counterText = `${todayVisitorCount} / ${totalVisitorCount}`;
-            const titleTextWidth = 45;
-            const counterTextWidth = 45 + ((counterText.length - 5) * 6);
-
-            const responseText = `
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${titleTextWidth + counterTextWidth}" height="20">
-                <linearGradient id="smooth" x2="0" y2="100%">
-                    <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
-                    <stop offset="1" stop-opacity=".1" />
-                </linearGradient>
-
-                <mask id="round">
-                    <rect width="${titleTextWidth + counterTextWidth}" height="20" rx="3" ry="3" fill="#fff" />
-                </mask>
-
-                <g mask="url(#round)">
-                    <rect width="${titleTextWidth}" height="20" fill="#555" />
-                    <rect x="${titleTextWidth}" width="${counterTextWidth}" height="20" fill="#79c83d" />
-                    <rect width="${titleTextWidth + counterTextWidth}" height="20" fill="url(#smooth)" />
-                </g>
-
-                <g fill="#fff" text-anchor="middle" font-family="Verdana,DejaVu Sans,Geneva,sans-serif" font-size="11">
-                    <text x="${(titleTextWidth / 2.0) + 1}" y="15" fill="#010101" fill-opacity=".3">${title}</text>
-                    <text x="${(titleTextWidth / 2.0) + 1}" y="14" fill="#fff">${title}</text>
-                    <text x="${titleTextWidth + (counterTextWidth / 2.0) - 1}" y="15" fill="#010101" fill-opacity=".3"> ${counterText} </text>
-                    <text x="${titleTextWidth + (counterTextWidth / 2.0) - 1}" y="14" fill="#fff"> ${counterText} </text>
-                </g>
-            </svg>
-            `;
-
             response.writeHead(200, { "Content-Type": "image/svg+xml" });
-            response.end(responseText);
+            response.end(createCounterSvg("방문자", `${todayVisitorCount} / ${totalVisitorCount}`));
         } else {
             response.statusCode = 404;
             response.end();
